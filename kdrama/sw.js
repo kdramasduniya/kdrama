ea5f0b0c561227037738ecde0e562a241d21b207
abbaskhanacc15401-glitch <abbaskhanacc15401@gmail.com>
2026-06-20 19:35:46 +0500

    Update sw.js

--- kdrama/sw.js
+++ kdrama/sw.js
@@ -1,18 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
     "zoneId": 11177235
-};
-self.lary = "";
-// importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
