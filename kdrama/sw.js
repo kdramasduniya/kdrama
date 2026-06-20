8ea9a418b8813efa40167a7112d0053ccf007eb5
abbaskhanacc15401-glitch <abbaskhanacc15401@gmail.com>
2026-06-20 17:49:12 +0500

    Add files via upload

--- kdrama/sw.js
+++ kdrama/sw.js
@@ -3,4 +3,16 @@ self.options = {
     "zoneId": 11177235
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+// importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')  // commented out for safety
+
+// minimal safe handlers (optional)
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  clients.claim();
+});
+self.addEventListener('fetch', event => {
+  // noop or simple network-first
+  event.respondWith(fetch(event.request).catch(()=> caches.match(event.request)));
+});
