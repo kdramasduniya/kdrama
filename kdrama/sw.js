b79b4b54eb991fa2aa5aaeaf03a8508d6eeb20b9
abbaskhanacc15401-glitch <abbaskhanacc15401@gmail.com>
2026-06-20 19:33:37 +0500

    Fix formatting and uncomment service worker script

--- kdrama/sw.js
+++ kdrama/sw.js
@@ -1,18 +1,18 @@
 self.options = {
     "domain": "3nbf4.com",
     "zoneId": 11177235
-}
-self.lary = ""
-// importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')  // commented out for safety
+};
+self.lary = "";
+// importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
 
-// minimal safe handlers (optional)
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
+
 self.addEventListener('activate', event => {
   clients.claim();
 });
+
 self.addEventListener('fetch', event => {
-  // noop or simple network-first
-  event.respondWith(fetch(event.request).catch(()=> caches.match(event.request)));
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
